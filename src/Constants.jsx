ccc87ceb6423d1661b714f4718073fa1702a8b2b
alonepranav <star.pranav111@gmail.com>
2023-12-30 10:37:42 +0530

    First Commit

--- src/Constants.jsx
+++ src/Constants.jsx
@@ -1,7 +1,6 @@
 export const MONGO_CONNECTION_STRING = process.env.MONGO_DB_STRING;
 // MONGO_DB_STRING=mongodb://127.0.0.1:27017/Task-Manager
 
-// export const BACKEND_API = process.env.BACKEND_API;
 // export const BACKEND_API = "http://localhost:3000/api";
 export const BACKEND_API = "https://task-manager-pranav.vercel.app/api";
 
